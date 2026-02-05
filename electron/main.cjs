6ebd800cf92488d56501113cc6a0200662fcbfb9
beanston <wrchoate@icloud.com>
2026-02-04 23:48:53 -0600

    teyhehe

--- electron/main.cjs
+++ electron/main.cjs
@@ -4,9 +4,10 @@ const { spawn } = require('child_process');
 const fs = require('fs');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-    app.quit();
-}
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+// if (require('electron-squirrel-startup')) {
+//     app.quit();
+// }
 
 let mainWindow;
 let serverProcess;
